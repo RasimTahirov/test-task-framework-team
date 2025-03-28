a7d9f70e1233d1194d02bf5261af00fd7f9161c6
Rasim <eferiums@yandex.ru>
2025-03-28 16:59:47 +0300

    final refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,42 +1,29 @@
-import {
-  useGetAuthorQuery,
-  useGetLocationQuery,
-  useGetPaintingsQuery,
-} from "../api/api";
-import Header from "../components/Header";
 import { useEffect, useState } from "react";
-import Search from "../components/Search";
-import PaintingsCards from "../components/PaintingsCards";
-import Pagination from "../components/Pagination";
-import { IPaintings, Theme } from "../types/types";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../store/store";
 import { setTheme } from "../store/slice";
+import { Theme } from "../types/types";
+import Header from "../components/Header";
+import Search from "../components/Search";
+import PaintingsCards from "../components/PaintingsCards";
+import Pagination from "../components/Pagination";
+import usePaintings from "../hooks/usePaintings";
 
 function App() {
-  const dispatch = useDispatch<AppDispatch>();
-
   const [pagination, setPagination] = useState(1);
   const [title, setTitle] = useState("");
 
-  const limit = 6;
+  const dispatch = useDispatch<AppDispatch>();
 
-  const { data: paintings = {} as { payload: IPaintings[]; total: number } } =
-    useGetPaintingsQuery({ _page: pagination, _limit: limit, title });
-  const { data: authors = [] } = useGetAuthorQuery();
-  const { data: locations = [] } = useGetLocationQuery();
-
-  const paintingsWithDetails = paintings?.payload?.map((painting) => {
-    const author = authors.find(({ id }) => id === painting.authorId);
-    const location = locations.find(({ id }) => id === painting.locationId);
-
-    return { ...painting, author, location };
-  });
+  const { paintingsWithDetails, paintings, limit } = usePaintings(
+    pagination,
+    title,
+  );
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme") as Theme | null;
     const theme = storedTheme || "light";
-    
+
     dispatch(setTheme(theme));
     document.documentElement.setAttribute("data-theme", theme);
   }, [dispatch]);
